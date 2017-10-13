e59acd24a39952820f106ba71d92a32f03636381
chrislai621 <kristy.lai621@gmail.com>
2017-10-13 15:42:21 +0800

    日期格式定義有誤

--- app/src/main/java/com/example/student/ce_refrigerator/EmptyData/food_list.java
+++ app/src/main/java/com/example/student/ce_refrigerator/EmptyData/food_list.java
@@ -1,32 +1,37 @@
 package com.example.student.ce_refrigerator.EmptyData;
 
+import java.util.Date;
+
 /**
  * Created by student on 2017/10/13.
  */
 
 public class food_list {
-  private   int id;//(主鍵)
-    private  String img_id;//(照片id)
-    private   int category_id;//(食材類別 FK category.id)
+    private int id;//(主鍵)
+    private String img_id;//(照片id)
+    private int category_id;//(食材類別 FK category.id)
     private int food_id;//(食物名稱 FK food.id)
-    private String purchase_date;//(採購日期)
-    private String expired_date;//(到期日期)
-    private String purchase_date2;//(轉換過採購日期)
-    private String expired_date2;//(轉換過到期日期)
+    private Date purchase_date;//(採購日期)
+    private Date expired_date;//(到期日期)
+    private int purchase_date2;//(轉換過採購日期)
+    private int expired_date2;//(轉換過到期日期)
     private double purchase_amount;//(採購金額)
     private int storage;//(存放位置)0冷藏1冷凍
     private String remark;//(備註)
 
-    public food_list(int id, String img_id, int category_id, int food_id, String expired_date, String expired_date2, double purchase_amount, int storage, String remark) {
+    public food_list(int id, String img_id, int category_id, int food_id, Date purchase_date, Date expired_date, double purchase_amount, int storage, String remark) {
+
         this.id = id;
         this.img_id = img_id;
         this.category_id = category_id;
         this.food_id = food_id;
+        this.purchase_date = purchase_date;
         this.expired_date = expired_date;
-        this.expired_date2 = expired_date2;
         this.purchase_amount = purchase_amount;
         this.storage = storage;
         this.remark = remark;
+        this.purchase_date2 = Integer.parseInt(purchase_date.toString().replace("/", "").replace(":", "").replace(" ", ""));
+        this.expired_date2 = Integer.parseInt(expired_date.toString().replace("/", "").replace(":", "").replace(" ", ""));
     }
 
     public int getId() {
@@ -61,35 +66,35 @@ public class food_list {
         this.food_id = food_id;
     }
 
-    public String getPurchase_date() {
+    public Date getPurchase_date() {
         return purchase_date;
     }
 
-    public void setPurchase_date(String purchase_date) {
+    public void setPurchase_date(Date purchase_date) {
         this.purchase_date = purchase_date;
     }
 
-    public String getExpired_date() {
+    public Date getExpired_date() {
         return expired_date;
     }
 
-    public void setExpired_date(String expired_date) {
+    public void setExpired_date(Date expired_date) {
         this.expired_date = expired_date;
     }
 
-    public String getPurchase_date2() {
+    public int getPurchase_date2() {
         return purchase_date2;
     }
 
-    public void setPurchase_date2(String purchase_date2) {
+    public void setPurchase_date2(int purchase_date2) {
         this.purchase_date2 = purchase_date2;
     }
 
-    public String getExpired_date2() {
+    public int getExpired_date2() {
         return expired_date2;
     }
 
-    public void setExpired_date2(String expired_date2) {
+    public void setExpired_date2(int expired_date2) {
         this.expired_date2 = expired_date2;
     }
 
