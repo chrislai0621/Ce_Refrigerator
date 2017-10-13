28d4703f4111e4ec99f97e352414e5f278eac1a3
chrislai621 <kristy.lai621@gmail.com>
2017-10-13 17:15:53 +0800

    建假資料，顯示在listview上

--- app/src/main/java/com/example/student/ce_refrigerator/Category.java
+++ app/src/main/java/com/example/student/ce_refrigerator/Category.java
@@ -1,13 +1,55 @@
 package com.example.student.ce_refrigerator;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.example.student.ce_refrigerator.EmptyData.category;
+
+import java.util.ArrayList;
 
 public class Category extends AppCompatActivity {
-
+    ArrayList<category> listCategory = new ArrayList<>();
+    ArrayAdapter<String> adapter;
+    ArrayList<String> forAdapter = new ArrayList<String>();
+    ListView listview ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_category);
+        getData();
+        initListView();
+    }
+
+    /**
+     * 取得大類資料
+     */
+    public  void getData()
+    {
+        for(int i=0 ; i<20;i++)
+        {
+            category AddCategory = new category(i,"名稱"+i);
+            listCategory.add(AddCategory);
+            forAdapter.add("名稱"+i);
+        }
+
+    }
+    public  void initListView()
+    {
+        listview =(ListView)findViewById(R.id.lvCategory);
+        adapter = new ArrayAdapter<String>(Category.this, android.R.layout.simple_list_item_1, forAdapter);
+        listview.setAdapter(adapter);
+        listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Intent it= new Intent(Category.this   ,SubCategory.class);
+                it.putExtra("ID",listCategory.get(i).getId());
+                startActivity(it);
+            }
+        });
     }
 }
